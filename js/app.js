a07259773a6b7261b1f6f19df6dca10322332cc4
RK <rakosolapov94@gmail.com>
2020-10-07 12:05:14 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { preventCtrl } from "./_preventOrientation.js";
-import GameCtrl from "./game.js";
-import { UICtrl } from "./ui.js";
+import { preventCtrl } from "js/_preventOrientation.js";
+import GameCtrl from "js/game.js";
+import { UICtrl } from "js/ui.js";
 
 const appInit = (function (GameCtrl, UICtrl) {
   function initEventListeners(UICtrl) {
